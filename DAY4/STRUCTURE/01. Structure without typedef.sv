2411278a9e4ccf6db908227abb0505eb917c346d
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-11-10 13:35:48 +0530

    Create 01. Structure without typedef.sv

---
+++ DAY4/STRUCTURE/01. Structure without typedef.sv
@@ -0,0 +1,18 @@
+// Structure without typedef
+module struct_no_typedef;
+  struct {
+    string name;
+    bit[31:0] salary;
+    integer id;
+  } employee;
+    
+  initial begin
+    employee.name = "Alexa";
+    employee.salary = 'h2536;
+    employee.id     = 'd1234;
+    $display("employee: %p", employee);
+    
+    // Accessing individual struct member
+    $display("employee: name = %s, salary = 0x%0h, id = %0d", employee.name, employee.salary, employee.id);
+  end
+endmodule

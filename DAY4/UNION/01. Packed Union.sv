52b2eadcba1c1dff4c830c0f85f9d0e719d2f6fc
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-11-16 14:28:12 +0530

    Update 01. Packed Union.sv

--- DAY4/UNION/01. Packed Union.sv
+++ DAY4/UNION/01. Packed Union.sv
@@ -3,7 +3,7 @@ module union_packed;
   bit[31:0] x;
   typedef union packed{
     int a;
-    logic[31:0] b;        // all members are in same bit width
+    logic[31:0] b;       // all members are in same bit width
     bit [31:0] c;
   } data;
   data d1;
@@ -13,8 +13,8 @@ module union_packed;
     x = d1.b;//read to b
     $display("x=%h",x);
     
-    d1.b = 32'h0000_ffff;//write to b
+    d1.b[31:16] = 16'h0000;//write to b //Only 31:16 bit is changed
     x = d1.c;//read to c
-    $display("x=%h",x);
+    $display("x=%h",x);  //Expected Output: 0000ffff
   end
 endmodule

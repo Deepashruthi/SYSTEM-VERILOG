ba5d85483dbd4107b9a99a986cc7e1f5b48c59a0
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-11-18 20:35:22 +0530

    Create 04. Forever inside class.sv

---
+++ DAY8/LOOPING STATEMENTS/04. Forever inside class.sv
@@ -0,0 +1,25 @@
+// Forever inside class
+class transaction;
+  int cnt;
+  task inc_cnt();
+    forever begin
+      $display("Value of count =%0d",cnt);
+      cnt++;
+      #5;
+    end
+  endtask
+endclass
+
+
+module forever_example;
+  transaction tr;
+  initial begin
+    tr = new();
+    tr.inc_cnt();
+  end
+  
+  initial begin
+    #100;
+    $finish;
+  end
+endmodule

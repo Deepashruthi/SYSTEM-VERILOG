c0412be4f4cbd5f6d318577bf259639447333701
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-11-20 01:03:29 +0530

    Create 02. Sum of N numbers in Dynamic Array.sv

---
+++ ASSIGNMENTS/PRACTICE_1/02. Sum of N numbers in Dynamic Array.sv
@@ -0,0 +1,17 @@
+// Sum of N numbers in Dynamic Array
+module sum_N_numbers;
+  int data[];
+  int result;
+  function int sum_N(int ip[]);
+    int sum = 0;
+    foreach(ip[i])
+      sum sum + ip[i];
+    return sum;
+  endfunction
+  initial begin
+    data = new[5];
+    data = '{1,2,3,4,5};
+    result = sum_N(data);
+    $display("Sum of N numbers is %d",result);
+  end
+endmodule

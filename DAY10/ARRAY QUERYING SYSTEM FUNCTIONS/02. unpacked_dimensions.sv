7626ac8e2546cdfaa1eb8629bf8b2ad054ba6250
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-11-25 22:57:11 +0530

    Update 02. unpacked_dimensions.sv

--- DAY10/ARRAY QUERYING SYSTEM FUNCTIONS/02. unpacked_dimensions.sv
+++ DAY10/ARRAY QUERYING SYSTEM FUNCTIONS/02. unpacked_dimensions.sv
@@ -3,7 +3,7 @@ module array_query2;
   typedef logic [15:0] mem_t [1:63][1:8]; 
   int myarr [2:0][3:0][2:0]; //returns 3 not include the data types
   logic [7:0][15:0] myarr1 [2:0][3:0][2:0]; 
-  var mem_t mem;
+  mem_t mem;
 
 initial begin
   $display("Unpacked dimesion of mem : %0d", $unpacked_dimensions(mem_t)); 

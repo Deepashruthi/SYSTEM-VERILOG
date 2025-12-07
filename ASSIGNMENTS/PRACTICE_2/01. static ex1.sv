9f6a8a4464aff1be31f39c6328e0bd820ea0a383
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-12-07 12:44:42 +0530

    Create 01. static ex1.sv

---
+++ ASSIGNMENTS/PRACTICE_2/01. static ex1.sv
@@ -0,0 +1,20 @@
+// static example 1
+class base;
+  static int i;
+  static function static get();
+    int a; // This also static bcoz its inside static method
+    a++;
+    i++;
+    $display(a);
+    $display(i);
+  endfunction
+endclass
+
+base b1, b2;
+module tb;
+  initial begin
+    b1.get();
+    b1.get();// static properties have a single memory location for all handles and can access without creating the objects
+    b2.get();
+  end
+endmodule

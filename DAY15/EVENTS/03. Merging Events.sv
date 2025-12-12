d962e1b99a77d2469180a5580742490582f635b0
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-12-12 16:35:44 +0530

    Create 03. Merging Events.sv

---
+++ DAY15/EVENTS/03. Merging Events.sv
@@ -0,0 +1,23 @@
+// Merging events
+module merge_events;
+  event a,b;
+  initial begin
+    fork
+    #50 -> a; 
+    #30 -> b;
+    #20 a = b;
+      begin 
+        wait(a.triggered);
+        $display("[%0t] event a is done",$time);
+      end    
+      begin 
+        wait(b.triggered);
+        $display("[%0t] event b is done ",$time);
+      end
+    join
+  end
+endmodule  
+
+//OUTPUT
+[30] event b is done 
+[30] event a is done

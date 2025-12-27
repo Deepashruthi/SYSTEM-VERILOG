c346edf87dd4829cd0c58a0d0d461c63375d1f9e
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-12-27 12:12:17 +0530

    Update 01. randomization example.sv

--- DAY17/Randomization/01. randomization example.sv
+++ DAY17/Randomization/01. randomization example.sv
@@ -13,3 +13,16 @@ module rand_methods;
       end
   end
 endmodule
+
+
+//OUTPUT
+addr1 = 4  addr2 = 3
+addr1 = 1  addr2 = 5
+addr1 = 7  addr2 = 4
+addr1 = 6  addr2 = 7
+addr1 = 7  addr2 = 0
+addr1 = 4  addr2 = 2
+addr1 = 7  addr2 = 1
+addr1 = 7  addr2 = 6
+addr1 = 4  addr2 = 2
+addr1 = 5  addr2 = 5

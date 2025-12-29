c9f08423155f0b22fa1f87ffb711289906672009
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-12-29 16:00:47 +0530

    Create 01. := operator.sv

---
+++ DAY18/WEIGHTED DISTRIBUTION/01. := operator.sv
@@ -0,0 +1,30 @@
+//Weighted distribution eg1
+class myWorld;  
+  rand bit [3:0] v1;  
+  constraint m_var { v1 dist { [2:5] := 10, 10 := 40, 12 := 20 }; }  
+endclass  
+
+module top;  
+  initial begin  
+    myWorld world = new();  
+
+    $display("----- Output for := operator -----");   
+    for (int i = 0; i < 10; i++) begin  
+      void'(world.randomize());  
+      $display("Iteration: %0d -----> v1 = %0d", i, world.v1);  
+    end  
+  end  
+endmodule  
+
+//OUTPUT
+----- Output for := operator -----
+Iteration: 0 -----> v1 = 10
+Iteration: 1 -----> v1 = 5
+Iteration: 2 -----> v1 = 5
+Iteration: 3 -----> v1 = 10
+Iteration: 4 -----> v1 = 10
+Iteration: 5 -----> v1 = 10
+Iteration: 6 -----> v1 = 12
+Iteration: 7 -----> v1 = 2
+Iteration: 8 -----> v1 = 12
+Iteration: 9 -----> v1 = 4

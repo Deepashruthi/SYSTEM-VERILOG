919a62e333c6581ea80744e1285a01f33f7e2569
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-12-29 10:33:50 +0530

    Update 08. std :: randomize inside the class.sv

--- DAY17/Randomization/08. std :: randomize inside the class.sv
+++ DAY17/Randomization/08. std :: randomize inside the class.sv
@@ -5,19 +5,19 @@ class std_random_class;
   constraint c1 { a>=2; a<=9;}
   
   function void display();
-    int c;
+    integer c;
     $display("----------------------------------------");
-    void'(std::randomize(this));
-    $display("Using std_randomize %0d,%0d",a,b);
-    if(randomize(a))
+    void'(std::randomize(this)); // local variable c is not randomized  // a and b need not be declared using rand and not use the global constraints
+    $display("Using std_randomize %0d,%0d,%0d",a,b,c);
+    if(randomize(a)) // randomize using the global constraint
       $display("randomize(a) %0d",a);
-    if(std::randomize(a))
+    if(std::randomize(a)) // randomized but not based on global constraint
       $display("using std_randomize(a) %0d",a);
     if(std::randomize(b))
       $display("using std_randomize b:%0d",b);
     if(std::randomize(c) with {c>1;c<4;});
     $display("using std_randomize with constraint ,c:%0d",c);
-    void'(this.randomize());
+    void'(this.randomize()); // randomize class properties a and b not c // a and b need to be declared as rand
     $display("this randomize %0d %0d %0d",a,b,c);
   endfunction
 endclass
@@ -35,21 +35,21 @@ endmodule
 
 //OUTPUT
 # KERNEL: ----------------------------------------
-# KERNEL: Using std_randomize 1497363586,5
+# KERNEL: Using std_randomize 1497363586,5,x
 # KERNEL: randomize(a) 6
 # KERNEL: using std_randomize(a) 484274802
 # KERNEL: using std_randomize b:5
 # KERNEL: using std_randomize with constraint ,c:2
 # KERNEL: this randomize 4 7 2
 # KERNEL: ----------------------------------------
-# KERNEL: Using std_randomize -1621255588,0
+# KERNEL: Using std_randomize -1621255588,0,x
 # KERNEL: randomize(a) 5
 # KERNEL: using std_randomize(a) -1608882576
 # KERNEL: using std_randomize b:2
 # KERNEL: using std_randomize with constraint ,c:3
 # KERNEL: this randomize 9 0 3
 # KERNEL: ----------------------------------------
-# KERNEL: Using std_randomize 1182761674,6
+# KERNEL: Using std_randomize 1182761674,6,x
 # KERNEL: randomize(a) 3
 # KERNEL: using std_randomize(a) -1520610950
 # KERNEL: using std_randomize b:3

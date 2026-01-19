c6b18b19f94c9fd3763286dbd525235d0996cda6
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-01-19 13:16:26 +0530

    Add scoreboard class for transaction verification

---
+++ VERIFICATION/D FlipFlop/07. Scoreboard.sv
@@ -0,0 +1,24 @@
+//Scoreboard
+class scoreboard;
+  transaction trans;
+  mailbox moni_to_scor;
+  event trigger;
+  
+  function new(mailbox moni_to_scor);
+    this.moni_to_scor = moni_to_scor;
+  endfunction
+  
+  task scor();
+    trans = new();
+    repeat(50) begin
+      moni_to_scor.get(trans);
+      trans.disp("SCOREBOARD");
+      
+      if((trans.rst == 1) && (trans.q == 0) || (trans.rst == 0) && (trans.q == trans.d))
+        $display("----------------PASS----------------");
+      else
+        $display("----------------FAIL----------------");
+      ->trigger;
+    end
+  endtask
+endclass

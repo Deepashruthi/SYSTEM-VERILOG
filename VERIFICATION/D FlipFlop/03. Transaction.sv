7959a2a0b0cf824c7cc36ed45a4c28134867578c
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-01-19 13:14:06 +0530

    Add transaction class for handling flip-flop operations

---
+++ VERIFICATION/D FlipFlop/03. Transaction.sv
@@ -0,0 +1,11 @@
+//Transaction
+class transaction;
+  bit rst;
+  rand bit d;
+  bit q;
+  
+  function void disp(string message);
+    $display("%s", message);
+    $display("Time = %0t | rst=%b | d=%b | q=%b",$time,rst,d,q);
+  endfunction
+endclass

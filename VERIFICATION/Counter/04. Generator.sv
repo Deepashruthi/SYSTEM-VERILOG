d235678f541128e2a04f578133954a52fc05191c
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-02-03 13:25:02 +0530

    Create 04. Generator.sv

---
+++ VERIFICATION/Counter/04. Generator.sv
@@ -0,0 +1,20 @@
+//Generator
+class generator;
+  transaction trans;
+  mailbox gen_to_driv;
+  event trigger;
+  
+  function new( mailbox gen_to_driv);
+    this.gen_to_driv = gen_to_driv;
+  endfunction
+  
+  task gen();
+    trans = new();
+    repeat(50) begin
+      void'(trans.randomize());
+      gen_to_driv.put(trans);
+      trans.disp("GENERATOR");
+      @trigger;
+    end
+  endtask
+endclass
